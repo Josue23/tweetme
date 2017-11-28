b354377a43e8e5b8b306769eaa18e9223133de50
josue23 <josuerodrigues@gmail.com>
2017-11-28 18:04:49 -0200

    model Tweet

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -2,12 +2,13 @@
 
 
 # from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 
 
 class Tweet(models.Model):
-	# user
+	# cada tweet é associado aum user
+	user      = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	content   = models.CharField(max_length=140)
 	updated   = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
