11b4912a10582942e0bab11da02fecaffa2e7956
josue <josue@jobconvo.com>
2017-11-29 21:26:14 -0200

    fix na view

--- src/tweetme/views.py
+++ src/tweetme/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+# retrieve
+# GET - Template home.html
+
+
 def home(request):
-	return render(request, "home.html", {})
+    return render(request, "home.html", {})
