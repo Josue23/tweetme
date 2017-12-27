455d3781be690c8f3bd4120b7489576a28e2ab2b
josue <josue@jobconvo.com>
2017-12-27 13:39:12 -0200

    jQuery search

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 User = get_user_model()
 
 class UserDisplaySerializer(serializers.ModelSerializer):
+    # SerializerMethodField - read-only field
     follower_count = serializers.SerializerMethodField()
     class Meta:
         model = User
